2572520004f55e600999fafa1c806a50409f745f
Meghan Guse <meghan.guse@gmail.com>
2017-07-16 17:30:47 -0500

    Choose your own adventure
    Comfort: 2/5: I can read the js, but have a hard time creating my own.
    For example, I really wanted to set it so that you had to select the
    buttons from top to bottom (it will currently take any order), but after
    an hour of googling, I couldn't come up with the correct wording to get
    the answer. I also couldn't get my stylesheet to override
    materialize.css
    
    Complete: 2/5: Couldn't get any of my styling to work, so I stopped
    adding to the stylesheet after a bit.

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,13 +1,90 @@
 console.log('hello')
 
+var firstSentence = 'You are the proud employee of the Krusty Krab';
+var secondSentence = 'You are the depressed employee of the Chum Bucket';
+var thirdSentence = 'You are forced to move in with your new roommate, Larry the Lobster';
+var fourthSentence = 'You are forced to move in with your new roommate, Sheldon J. Plankton';
+var fifthSentence = 'You eventually move into a high rise anchor, surrounded by famous fry cooks.';
+var sixthSentence = 'You are kicked out after blowing too many bubbles. Now homeless, you live in the dumpster outside of the Krusty Krab'
+var seventhSentence = 'Eventually, you find a new career as a professional jellyfisher.';
+var eighthSentence = 'You end up getting sent to the hospital after a tragic karate accident.';
+
 window.onload = function(event) {
-	var nameInput = document.getElementById('first_name');
+	var nameInput = document.getElementById('first_name');''
 	var nameBtn = document.getElementById('name_btn');
+	var firstChoice = document.getElementById('choice-one');
+	var secondChoice = document.getElementById('choice-two');
+	var thirdChoice = document.getElementById('choice-three');
+	var fourthChoice = document.getElementById('choice-four');
+	var fifthChoice = document.getElementById('choice-five');
+	var sixthChoice = document.getElementById('choice-six');
+	var seventhChoice = document.getElementById('choice-seven');
+	var eighthChoice = document.getElementById('choice-eight');
+
+	firstChoice.onclick = function(event) {
+		createRow(firstSentence);
+		document.getElementById('first-step').style.display = 'none';
+	}
+
+	secondChoice.onclick = function(event) {
+		createRow(secondSentence);
+		document.getElementById('first-step').style.display = 'none';
+	}
+
+	thirdChoice.onclick	= function(event) {
+		createRow(thirdSentence);
+		document.getElementById('second-step').style.display = 'none';
+	}
+
+	fourthChoice.onclick = function(event) {
+		createRow(fourthSentence);
+		document.getElementById('second-step').style.display = 'none';
+	}
+
+	fifthChoice.onclick = function(event) {
+		createRow(fifthSentence);
+		document.getElementById('third-step').style.display = 'none';
+	}
+
+	sixthChoice.onclick = function(event) {
+		createRow(sixthSentence);
+		document.getElementById('third-step').style.display = 'none';
+	}
+
+	seventhChoice.onclick = function(event) {
+		createRow(seventhSentence);
+		document.getElementById('fourth-step').style.display = 'none';
+	}
+
+	eighthChoice.onclick = function(event) {
+		createRow(eighthSentence);
+		document.getElementById('fourth-step').style.display = 'none';
+	}
 
 	nameBtn.onclick = function(event) {
 		// console.log(event);
 		console.log(nameInput.value);
+		createRow('Arr, welcome aboard, ' + nameInput.value);
 	}
 
 	console.log(nameInput, nameBtn);
+}
+
+
+//this function creates and adds rows to our app
+function createRow(words) {
+	console.log(words);
+	//we need a new div
+	var row = document.createElement('div');
+	var col = document.createElement('div');
+	var p = document.createElement('p');
+	p.innerHTML = words;
+	row.append(col);
+	row.className = 'row';
+	col.className = 'cols12';
+	col.append(p);
+	console.log(row);
+	//append the whole thing to app
+	var app = document.getElementById('app');
+	app.append(row);
 }
